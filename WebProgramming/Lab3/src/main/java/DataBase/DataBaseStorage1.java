c3e884299f30f705f709b7684189ef6526764555
Tolia <2398768715@qq.com>
2023-10-16 22:52:56 +0300

    update

--- WebProgramming/Lab3/src/main/java/DataBase/DataBaseStorage1.java
+++ WebProgramming/Lab3/src/main/java/DataBase/DataBaseStorage1.java
@@ -1,35 +1,35 @@
 package DataBase;
 
-import Data.DotsTable;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@NoArgsConstructor
-public class DataBaseStorage1 implements Serializable {
-
-    public void addNewDot(DotsTable dot) throws Exception{
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        Class.forName("org.postgresql.Driver");
-        try(Connection connection = DriverManager.getConnection(url,"postgres","123456")){
-            try(PreparedStatement ps= connection.prepareStatement(
-                    "INSERT INTO web_lab3.dotstable(hit,r,time,x,y) values (?,?,?,?,?)"
-            )) {
-                ps.setBoolean(1,dot.isHit());
-                ps.setDouble(2,dot.getR());
-                ps.setObject(3,dot.getTime());
-                ps.setDouble(4,dot.getX());
-                ps.setDouble(5,dot.getY());
-                ps.executeUpdate();
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-}
+//import Data.DotsTable;
+//import lombok.NoArgsConstructor;
+//
+//import java.io.Serializable;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//
+//@NoArgsConstructor
+//public class DataBaseStorage1 implements Serializable {
+//
+//    public void addNewDot(DotsTable dot) throws Exception{
+//        String url = "jdbc:postgresql://localhost:5432/postgres";
+//        Class.forName("org.postgresql.Driver");
+//        try(Connection connection = DriverManager.getConnection(url,"postgres","123456")){
+//            try(PreparedStatement ps= connection.prepareStatement(
+//                    "INSERT INTO web_lab3.dotstable(hit,r,time,x,y) values (?,?,?,?,?)"
+//            )) {
+//                ps.setBoolean(1,dot.isHit());
+//                ps.setDouble(2,dot.getR());
+//                ps.setObject(3,dot.getTime());
+//                ps.setDouble(4,dot.getX());
+//                ps.setDouble(5,dot.getY());
+//                ps.executeUpdate();
+//            }catch (SQLException e){
+//                e.printStackTrace();
+//            }
+//        }catch (SQLException e){
+//            e.printStackTrace();
+//        }
+//    }
+//}
