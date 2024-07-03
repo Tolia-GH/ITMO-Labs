81b50dcd4a2ec6907277a4bc9b3332b32e4cb7d7
Tolia <2398768715@qq.com>
2024-07-03 15:36:25 +0800

    Update JwtRequestFilter.java

--- BusinessLogic/Lab2/Lab/src/main/java/com/blps/lab1/utils/JwtRequestFilter.java
+++ BusinessLogic/Lab2/Lab/src/main/java/com/blps/lab1/utils/JwtRequestFilter.java
@@ -16,31 +16,31 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class JwtRequestFilter extends OncePerRequestFilter {
+public class JwtRequestFilter extends OncePerRequestFilter { // OncePerRequestFilter: Each request will go through filter only once
     @Autowired
-    private AccountsDetailService accountsDetailService;
+    private AccountsDetailService accountsDetailService; // to load user by account's email
     @Autowired
-    private JwtUtil jwtUtil;
+    private JwtUtil jwtUtil; // for generating, analyzing and authenticating JWT
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
+        final String authHeader = request.getHeader("Authorization"); // to get JWT from Http request header of "Authorization"
 
         String username = null;
         String jwt = null;
 
         String email = null;
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            jwt = authHeader.substring(7); // Take substring after 'Bearer '(7 symbols)
-            username = jwtUtil.extractUsername(jwt);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) { // to check whether the Http request header starts with "Bearer ", which is a standard format of JWT
+            jwt = authHeader.substring(7); // Take substring after 'Bearer '(7 symbols) and get jwt
+            username = jwtUtil.extractUsername(jwt); // from jwt get username
         }
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.accountsDetailService.loadUserByUsername(username);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) { // when username is not null and there is no authentication info in Security context
+            UserDetails userDetails = this.accountsDetailService.loadUserByUsername(username); // load user's detail info by using accountsDetailService
 
-            if (jwtUtil.validateToken(jwt, userDetails)) {
+            if (jwtUtil.validateToken(jwt, userDetails)) { // check whether JWT token is expired, if not, make user authenticated
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
