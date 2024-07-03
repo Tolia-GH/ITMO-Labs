343610e908c277c1ff7184bf00e2ba8121fc2685
Tolia <2398768715@qq.com>
2024-07-03 11:08:16 +0800

    Update SecurityConfig.java
    已解决的问题：
    - [x] 授权登录的问题，但需要注意的是，如果密码使用明文存储在数据库中，数据库中的密码栏需要在密码前加入`{noop}`，例如明文的密码：123456在数据库中应存为`{noop}123456`
    
    待解决的问题：
    - [ ] PasswordEncoder 配置问题

--- BusinessLogic/Lab2/Lab/src/main/java/com/blps/lab1/config/SecurityConfig.java
+++ BusinessLogic/Lab2/Lab/src/main/java/com/blps/lab1/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.blps.lab1.service.AccountsDetailService;
 import com.blps.lab1.utils.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -30,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").anonymous()
+                .antMatchers("/auth/signIn", "/auth/signUp").anonymous()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
