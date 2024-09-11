a756e0937bda8c4f16a418279d411774d7a71b01
Tolia <2398768715@qq.com>
2024-09-11 19:17:17 +0300

    update

---
+++ MobileApp/SmartHomeMobileApp/app/src/main/java/com/itmo/smarthome/smarthomemobile/app/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.itmo.smarthome.smarthomemobile.app.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
