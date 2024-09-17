fbd85e69edd4a015ea2071471d2bcaeb7e789211
Tolia <2398768715@qq.com>
2024-09-17 22:00:52 +0300

    mobile: login

--- MobileApp/SmartHomeMobileApp/app/src/main/java/com/itmo/smarthome/smarthomemobile/app/LoginActivity.kt
+++ MobileApp/SmartHomeMobileApp/app/src/main/java/com/itmo/smarthome/smarthomemobile/app/LoginActivity.kt
@@ -1,6 +1,10 @@
 package com.itmo.smarthome.smarthomemobile.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -8,6 +12,28 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        supportActionBar?.hide()
+        supportActionBar?.hide() // hide action bar on the top of app
+
+        val etUsername: EditText = findViewById(R.id.et_login_username)
+        val etPassword: EditText = findViewById(R.id.et_login_password)
+        val btnLogin: Button = findViewById(R.id.bt_login)
+        val btnRegister: Button = findViewById(R.id.bt_register)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                // 处理登录逻辑
+                Toast.makeText(this, "Welcome! $username", Toast.LENGTH_SHORT).show()
+                // 在这里添加验证逻辑
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
 }
