44311bf9ad99f467359d730e12bc8f912f2489fc
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2024-10-07 18:18:55 +0300

    update

--- InfoSecurity/Lab1Playfair/InfoSecurity Lab1/Playfair.py
+++ InfoSecurity/Lab1Playfair/InfoSecurity Lab1/Playfair.py
@@ -1,14 +1,14 @@
-# generate key matrix
+# function for generating key matrix
 def generate_key_matrix(key):
-    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"  # J和I合并
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"  # combine 'J' and 'I' together
     key = key.upper()
-    key = ''.join(sorted(set(key.upper()), key=lambda x: key.index(x)))  # 去除重复字母
+    key = ''.join(sorted(set(key.upper()), key=lambda x: key.index(x)))  # remove repeat letter in key
     key_matrix = []
     key_matrix_row = []
     used = set()
 
     for letter in key:
-        if letter not in used and letter != 'J':  # 忽略 'J'
+        if letter not in used and letter != 'J':  # ignore 'J'
             key_matrix_row.append(letter)
             used.add(letter)
         if key_matrix_row.__len__() == 5:
@@ -26,7 +26,7 @@ def generate_key_matrix(key):
     return key_matrix
 
 
-# find position of character in key matrix
+# function to find position of letter in key matrix
 def find_position(key_matrix, letter):
     for row in range(5):
         for col in range(5):
@@ -34,100 +34,170 @@ def find_position(key_matrix, letter):
                 return row, col
 
 
+# function to judge whether a character is an english letter
 def is_english_letter(char):
     return 'A' <= char <= 'Z' or 'a' <= char <= 'z'
 
 
+# function to transform text pair matrix to string
+def text_pairs_to_string(text_pairs):
+    text = ""
+    for text_pair in text_pairs:
+        for char in text_pair:
+            text += char
+    return text
+
+
+# function to print matrix
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+
+# function to get english letters from pairs
+def get_letter_from_pair(text_pair):
+    pair_letter = []
+    for letter in text_pair:
+        if is_english_letter(letter):
+            pair_letter.append(letter)
+    return pair_letter
+
+
+# function to preprocess_text
 # preprocess
 def preprocess_text(text):
-    text = text.replace("J", "I").replace("j", "i").replace(',', '').replace('.','').replace(' ','')  # replace J with I and remove spaces
-    processed_text = ""
+    text = text.replace("J", "I").replace("j", "i")  # replace J with I and remove spaces
+    processed_text_pairs = []
+    pair = []
+    pair_letter = []
     i = 0
 
     while i < len(text):
-        pair_l = text[i]
-        pair_r = text[i + 1] if (i + 1) < len(text) else 'X'  # add 'X' if len(text)%2 == 1
+        pair.append(text[i])
+        if is_english_letter(text[i]):
+            pair_letter.append(text[i])
+        if pair_letter.__len__() == 2:
+            if pair_letter[0] == pair_letter[1]: # if there are two same letters in one pair, then add 'X' between them
+                pair.remove(pair_letter[1])
+                pair_letter.remove(text[i])
+                pair.append('X')
+                processed_text_pairs.append(pair)
+                pair_letter = []
+                pair = []
+                i -= 1
+            else:
+                processed_text_pairs.append(pair)
+                pair_letter = []
+                pair = []
+        if i == len(text) - 1 and pair_letter.__len__() == 1: # if in the end the length of text % 2 == 1, then add 'X' in the end
+            pair.append('X')
+            processed_text_pairs.append(pair)
 
-        if pair_l == pair_r:  # if pair_l == pair_r, add 'X' to pairs
-            processed_text += pair_l + 'X'
-            i += 1
-        else:
-            processed_text += pair_l + pair_r
-            i += 2
+        i += 1
 
-    if len(processed_text) % 2 != 0:
-        processed_text += 'X'
-
-    return processed_text
+    return processed_text_pairs
 
 
 # encrypt process
 def encrypt(text, key_matrix):
-    encrypted_text = ""
-    text = preprocess_text(text)  # 预处理文本
+    encrypted_text_pairs = []
+    preprocessed_text_pairs = preprocess_text(text)  # preprocess plaint text
 
-    print("processed text:", text)
+    for preprocessed_text_pair in preprocessed_text_pairs:
+        preprocessed_pair_letter = get_letter_from_pair(preprocessed_text_pair)
+        row1, col1 = find_position(key_matrix, preprocessed_pair_letter[0])
+        row2, col2 = find_position(key_matrix, preprocessed_pair_letter[1])
 
-    for i in range(0, len(text), 2):
-        row1, col1 = find_position(key_matrix, text[i])
-        row2, col2 = find_position(key_matrix, text[i + 1])
+        encrypted_pair_letter = ['', '']
+        if row1 == row2:  # if same row
+            encrypted_pair_letter[0] = key_matrix[row1][(col1 + 1) % 5]
+            encrypted_pair_letter[1] = key_matrix[row2][(col2 + 1) % 5]
+        elif col1 == col2:  # if same col
+            encrypted_pair_letter[0] = key_matrix[(row1 + 1) % 5][col1]
+            encrypted_pair_letter[1] = key_matrix[(row2 + 1) % 5][col2]
+        else:  # if square
+            encrypted_pair_letter[0] = key_matrix[row1][col2]
+            encrypted_pair_letter[1] = key_matrix[row2][col1]
 
-        if row1 == row2:  # same row
-            pair_l = key_matrix[row1][(col1 + 1) % 5]
-            pair_r = key_matrix[row2][(col2 + 1) % 5]
-        elif col1 == col2:  # same col
-            pair_l = key_matrix[(row1 + 1) % 5][col1]
-            pair_r = key_matrix[(row2 + 1) % 5][col2]
-        else:  # square
-            pair_l = key_matrix[row1][col2]
-            pair_r = key_matrix[row2][col1]
+        # make the capitalization the same as plaint text
+        if preprocessed_pair_letter[0].islower():
+            encrypted_pair_letter[0] = encrypted_pair_letter[0].lower()
+        if preprocessed_pair_letter[1].islower():
+            encrypted_pair_letter[1] = encrypted_pair_letter[1].lower()
 
-        if text[i].islower():
-            pair_l = pair_l.lower()
-        if text[i+1].islower():
-            pair_r = pair_r.lower()
-        encrypted_text += pair_l + pair_r
+        # set encrypted text pair matrix
+        encrypted_text_pair = []
+        letter_count = 0
+        for letter in preprocessed_text_pair:
+            if is_english_letter(letter):
+                encrypted_text_pair.append(encrypted_pair_letter[letter_count])
+                letter_count += 1
+            else:
+                encrypted_text_pair.append(letter)
+        encrypted_text_pairs.append(encrypted_text_pair)
 
-    return encrypted_text
+    return encrypted_text_pairs
 
 
 # decrypt process
-def decrypt(ciphertext, key_matrix):
-    decrypted_text = ""
+def decrypt(encrypted_text_pairs, key_matrix):
+    decrypted_text_pairs = []
 
-    for i in range(0, len(ciphertext), 2):
-        row1, col1 = find_position(key_matrix, ciphertext[i])
-        row2, col2 = find_position(key_matrix, ciphertext[i + 1])
+    for encrypted_text_pair in encrypted_text_pairs:
+        encrypted_pair_letter = get_letter_from_pair(encrypted_text_pair)
+        row1, col1 = find_position(key_matrix, encrypted_pair_letter[0])
+        row2, col2 = find_position(key_matrix, encrypted_pair_letter[1])
 
-        if row1 == row2:  # same row
-            pair_l = key_matrix[row1][(col1 - 1) % 5]
-            pair_r = key_matrix[row2][(col2 - 1) % 5]
-        elif col1 == col2:  # same col
-            pair_l = key_matrix[(row1 - 1) % 5][col1]
-            pair_r = key_matrix[(row2 - 1) % 5][col2]
-        else:  # square
-            pair_l = key_matrix[row1][col2]
-            pair_r = key_matrix[row2][col1]
+        decrypted_pair_letter = ['', '']
+        if row1 == row2:  # if same row
+            decrypted_pair_letter[0] = key_matrix[row1][(col1 - 1) % 5]
+            decrypted_pair_letter[1] = key_matrix[row2][(col2 - 1) % 5]
+        elif col1 == col2:  # if same col
+            decrypted_pair_letter[0] = key_matrix[(row1 - 1) % 5][col1]
+            decrypted_pair_letter[1] = key_matrix[(row2 - 1) % 5][col2]
+        else:  # if square
+            decrypted_pair_letter[0] = key_matrix[row1][col2]
+            decrypted_pair_letter[1] = key_matrix[row2][col1]
 
-        if ciphertext[i].islower():
-            pair_l = pair_l.lower()
-        if ciphertext[i+1].islower():
-            pair_r = pair_r.lower()
-        decrypted_text += pair_l + pair_r
+        if encrypted_pair_letter[0].islower():
+            decrypted_pair_letter[0] = decrypted_pair_letter[0].lower()
+        if encrypted_pair_letter[1].islower():
+            decrypted_pair_letter[1] = decrypted_pair_letter[1].lower()
 
-    decrypted_text = remove_x(decrypted_text)
+        decrypted_text_pair = []
+        letter_count = 0
+        for letter in encrypted_text_pair:
+            if is_english_letter(letter):
+                decrypted_text_pair.append(decrypted_pair_letter[letter_count])
+                letter_count += 1
+            else:
+                decrypted_text_pair.append(letter)
+        decrypted_text_pairs.append(decrypted_text_pair)
 
-    return decrypted_text
+    decrypted_text_pairs = remove_x(decrypted_text_pairs)
+
+    return decrypted_text_pairs
 
 
-def remove_x(decrypted_text):
-    for i in range(0, len(decrypted_text) - 2):
-        if decrypted_text[i].upper() == decrypted_text[i + 2].upper() and decrypted_text[i].upper() == 'X':
-            decrypted_text = decrypted_text[:i] + decrypted_text[i + 2:]
-        i += 1
-    if decrypted_text[-1].upper() == 'X':
-        decrypted_text = decrypted_text[:-1]
-    return decrypted_text
+# function for removing additional 'X' in result during preprocess and encryption
+def remove_x(decrypted_text_pairs):
+    res_text_pairs = []
+    for i in range(len(decrypted_text_pairs) - 1):
+        text_pair = decrypted_text_pairs[i]
+        text_pair_next = decrypted_text_pairs[i + 1]
+        pair_letter = get_letter_from_pair(text_pair)
+        pair_letter_next = get_letter_from_pair(text_pair_next)
+
+        if pair_letter[0] == pair_letter_next[0] and pair_letter[1] == 'X':
+            text_pair.remove('X')
+
+        res_text_pairs.append(text_pair)
+
+    if decrypted_text_pairs[-1][-1] == 'X':
+        decrypted_text_pairs[-1].remove('X')
+        res_text_pairs.append(decrypted_text_pairs[-1])
+
+    return res_text_pairs
 
 
 # read from file
@@ -142,23 +212,41 @@ def write_file(filename, content):
         file.write(content)
 
 
+# def compare_strings(str1, str2):
+#     # 确保两个字符串的长度一致
+#     if len(str1) != len(str2):
+#         raise ValueError("String lengths do not match")
+#
+#     # 逐字符比较并统计差异字符数量
+#     diff_count = 0
+#     for char1, char2 in zip(str1, str2):
+#         if char1 != char2:
+#             diff_count += 1
+#
+#     return diff_count
+
+
 # main function
 if __name__ == "__main__":
-    #key_input = input("Please input keyword：")
-    key_input = "SECRETKEY"
+    key_input = input("Please input keyword：")
+    # key_input = "SECRETKEY"
     matrix = generate_key_matrix(key_input)
+
+    print("Generated key matrix: ")
+    print_matrix(matrix)
 
     # 从read plain text from file
     plaintext = read_file('plaintext.txt')
+    print(f"Plain text: \n{plaintext}")
 
     # encrypt text
-    text_encrypted = encrypt(plaintext, matrix)
-    write_file('encrypted.txt', text_encrypted)
+    text_pairs_encrypted = encrypt(plaintext, matrix)
+    write_file('encrypted.txt', text_pairs_to_string(text_pairs_encrypted))
 
-    print(f"encrypted text: {text_encrypted}")
+    print(f"Encrypted text: \n{text_pairs_to_string(text_pairs_encrypted)}")
 
     # decrypt text
-    text_decrypted = decrypt(text_encrypted, matrix)
-    write_file('decrypted.txt', text_decrypted)
+    text_pairs_decrypted = decrypt(text_pairs_encrypted, matrix)
+    write_file('decrypted.txt', text_pairs_to_string(text_pairs_decrypted))
 
-    print(f"decrypted text: {text_decrypted}")
+    print(f"Decrypted text: \n{text_pairs_to_string(text_pairs_decrypted)}")
