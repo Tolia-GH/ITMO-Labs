6cb6c4c92a81456f9daea81afc5a4c33a2aea1bf
Tolia <2398768715@qq.com>
2024-12-02 17:41:51 +0300

    update

--- InfoSecurity/Lab2.1/main.py
+++ InfoSecurity/Lab2.1/main.py
@@ -1,24 +1,16 @@
+
 import math
 
-if __name__ == '__main__':
-    N = 65815671868057
-    e = 7423489
-    C = [
-        38932868535359
-    ]
 
+# Функция для нахождения простых чисел p и q методом квадратичных корней
+def find_prime_factors(N):
+    # Начальное приближение корня
     n = int(math.sqrt(N)) + 1
-
-    b = 2
-    a = N
-    p = 0
-    q = 0
 
     i = 0
     while True:
-        i = i + 1
+        i += 1
         t = n + i
-        d = a ^ b
         w = t ** 2 - N
         sqrt_w = math.sqrt(w)
         if sqrt_w % 1 == 0:
@@ -26,16 +18,55 @@ if __name__ == '__main__':
             p = t + sqrt_w
             q = t - sqrt_w
             print(f"p={p}, q={q}")
-            break
+            return p, q
         else:
-            print("error, keep searching for p and q")
-    phi = round((p - 1) * (q - 1))
-    d = pow(e, -1, phi)
+            print("error")
 
+
+# Функция для расшифровки сообщения
+def decrypt_message(C, d, N):
     result = ""
     for i in C:
+        # Расчет m = C^d mod N
         m = pow(int(i), d, N)
+        # Преобразование числа в текст (кодировка cp1251)
         part = m.to_bytes(4, byteorder='big').decode('cp1251')
         print(f'{i}^{d} mod {N} = {m} => text({m}) = {part}')
         result += part
+    return result
+
+
+if __name__ == '__main__':
+    # Исходные данные
+    N = 48992988576733  # Модуль N
+    e = 4545733  # Открытая экспонента e
+    C = [  # Зашифрованные данные
+        12530303611339,
+        47274247086952,
+        20068556933394,
+        41300245344157,
+        27564916776233,
+        45997492729411,
+        11416336760074,
+        17516700753417,
+        10586755223028,
+        5642378694993,
+        17949047899806,
+        13276902592875
+    ]
+
+    # Нахождение простых чисел p и q
+    p, q = find_prime_factors(N)
+
+    # Вычисление функции Эйлера
+    phi = round((p - 1) * (q - 1))
+
+    # Вычисление закрытой экспоненты d
+    d = pow(e, -1, phi)
+
+    # Расшифровка сообщения
+    result = decrypt_message(C, d, N)
+
+    # Печать результата
     print(f"result = {result}")
+
