1d581a2ea6133bce10f628ccdff6d3413261c534
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2025-02-17 06:46:39 +0300

    Update HelloWorldResource.java

--- SOA/Lab2/Service1/src/main/java/se/ifmo/resource/HelloWorldResource.java
+++ SOA/Lab2/Service1/src/main/java/se/ifmo/resource/HelloWorldResource.java
@@ -7,8 +7,6 @@ import jakarta.ws.rs.core.Response;
 
 @Path("hello")
 public class HelloWorldResource {
-
-
     @GET
     public Response hello(@QueryParam("name") String name) {
         if ((name == null) || name.trim().isEmpty()) {
@@ -18,7 +16,5 @@ public class HelloWorldResource {
                 .ok(name)
                 .build();
     }
-
-    
 
 }
