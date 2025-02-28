c280b3579cdf31c5705405f2717485ec843acaad
Tolia <2398768715@qq.com>
2025-02-28 09:59:40 +0300

    frontend delete useless file

--- SOA/Lab2/demo_front/my-app/src/components/App.jsx
+++ SOA/Lab2/demo_front/my-app/src/components/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import {Route,Routes} from 'react-router-dom';
-import {LoginPage} from "./login/login";
+
 import {Main} from "./main/main";
-import {Register} from "./register/register";
+
 class App extends React.Component {
     render(){
         return(
             <div>
                 <Routes>
-                    <Route path="/" element={<LoginPage/>}/>
-                    <Route path={"/main"} element={<Main/>}/>
-                    <Route path={"/register"} element={<Register/>}/>
+                    <Route path="/" element={<Main/>}/>
                 </Routes>
             </div>
         )
--- SOA/Lab2/demo_front/my-app/src/components/main/main.jsx
+++ SOA/Lab2/demo_front/my-app/src/components/main/main.jsx
@@ -1,9 +1,8 @@
-import {MainForm} from "./mainForm";
-import {MainGraph} from "./mainGraph";
+
 import React from "react";
 import '../../assets/css/page_style.css'
 import {Link} from "react-router-dom";
-import ResultTable from "./mainResult";
+
 
 export class Main extends React.Component {
     render() {
@@ -14,13 +13,10 @@ export class Main extends React.Component {
                     <a id={"fork_me"} href={"https://github.com/Tolia-GH/ITMO-PE"}>
                         View on GitHub
                     </a>
-                    <h1 id={"web_title"}>Lab4</h1>
+                    <h1 id={"web_title"}>Lab2</h1>
                     <h2 id={"web_author"}>P32131 Чжоу Хунсян 13109</h2>
                 </div>
                 <Link className={"link"} to={"/"}><button id={"back"} className={"click_button"}>Back</button></Link>
-                <MainGraph/>
-                <MainForm/>
-                <ResultTable/>
             </div>
         )
     }
