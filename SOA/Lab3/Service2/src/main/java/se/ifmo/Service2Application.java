a2015df203d32f303aa26dd2ffbde9f9a532fe39
Tolia <2398768715@qq.com>
2025-12-05 00:36:07 +0300

    updtae

--- SOA/Lab3/Service2/src/main/java/se/ifmo/Service2Application.java
+++ SOA/Lab3/Service2/src/main/java/se/ifmo/Service2Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // 启用服务发现
 public class Service2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
