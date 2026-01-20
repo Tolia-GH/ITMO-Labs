de64bb3e3f04d5aa0ee3c819e6f65f5c63263cb9
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2026-01-20 22:11:17 +0300

    SOA-Lab4: Ignore SSL verify between Services

--- SOA/Lab4/Service2/src/main/java/se/ifmo/config/RestTemplateConfig.java
+++ SOA/Lab4/Service2/src/main/java/se/ifmo/config/RestTemplateConfig.java
@@ -3,47 +3,28 @@ package se.ifmo.config;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.SecureRandom;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() throws Exception {
-        // 使用 InputStream 读取 classpath 下的资源文件
-        InputStream trustStoreInputStream = getClass().getClassLoader().getResourceAsStream("cacerts.p12");
+    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        // 创建信任所有证书的 SSLContext
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial(null, (chain, authType) -> true)
+                .build();
 
-        if (trustStoreInputStream == null) {
-            throw new FileNotFoundException("Truststore file not found in the classpath.");
-        }
-
-        String trustStorePassword = "changeit";
-
-        KeyStore trustStore = KeyStore.getInstance("PKCS12");
-
-        // 从 InputStream 加载 Truststore
-        try (trustStoreInputStream) {
-            trustStore.load(trustStoreInputStream, trustStorePassword.toCharArray());
-        }
-
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        tmf.init(trustStore);
-
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
-
-        // 配置 HttpClient 忽略主机名验证
+        // 配置 HttpClient 使用该 SSLContext，并忽略主机名验证
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setSSLContext(sslContext)
                 .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
